f0e0ff08529f0c1160aa38933665c60c322d7030
sivakhno <sivakhno@illumina.com>
2017-03-15 15:54:00 +0000

    adapt CheckNonDiploidMAFs threshold

--- Src/Canvas/CanvasSomaticCaller/SomaticCaller.cs
+++ Src/Canvas/CanvasSomaticCaller/SomaticCaller.cs
@@ -1957,7 +1957,8 @@ namespace CanvasSomaticCaller
         /// </summary>
         protected void CheckNonDiploidMAFs(List<CanvasSegment> canvasSegments)
         {
-            const double diploidMAF = 0.45;
+            const double diploidMAF = 0.41;
+            const double diploidMAFMean = 0.44;
             double minSizeMAFs = 1000; // to approximate chopiness for 100 to 500kb regions
             const int tStatisticsSizeThreshold = 20;
 
@@ -1975,7 +1976,7 @@ namespace CanvasSomaticCaller
                 return;
             double tTestStatistics = CanvasCommon.Utilities.tTest(nonDiploidMAFs, diploidMAF);
             Console.WriteLine($">>> CheckNonDiploidMAFs t-test statistics {tTestStatistics}");
-            if (Math.Abs(tTestStatistics) < 3.733) // for alpha = 0.001 and 15 degrees of freedom
+            if (Math.Abs(tTestStatistics) < 3.733 | nonDiploidMAFs.Average() > diploidMAFMean) // for alpha = 0.001 and 15 degrees of freedom
             {
                 this.Model.Purity = 1.0;
                 this.Model.Ploidy = 2.0;
