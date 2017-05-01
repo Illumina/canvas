456bfa0c5e05728e81b6b7b9c5fde4cd28f73b2c
Stephen Tanner <stanner@illumina.com>
2017-05-01 13:11:01 -0700

    Fix up CanvasWrapper to have same version as its peers

--- Src/Canvas/Canvas.Wrapper/Wrapper/Properties/AssemblyInfo.cs
+++ Src/Canvas/Canvas.Wrapper/Wrapper/Properties/AssemblyInfo.cs
@@ -7,9 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Wrapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Wrapper")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")] // AssemblyVersion,AssemblyFileVersion,AssemblyCompany,AssemblyCopyright are auto-populated by TeamCtiy build
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
