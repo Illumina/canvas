07370e33ea0203df2ab97318c8a5471f26aaf4e0
Stephen Tanner <stanner@illumina.com>
2017-07-11 13:08:08 -0700

    Use a dummy settings instance instead of a special class

--- Src/Canvas/Canvas/Canvas.net45/ModeLauncher.cs
+++ Src/Canvas/Canvas/Canvas.net45/ModeLauncher.cs
@@ -57,7 +57,7 @@ namespace Canvas
                     try
                     {
                         var workerDirectory = new DirectoryLocation(Isas.Framework.Utilities.Utilities.GetAssemblyFolder(typeof(ModeLauncher)));
-                        var executableProcessor = new NullExecutableProcessor(logger, workerDirectory);
+                        var executableProcessor = new ExecutableProcessor((ISampleSettings)(new SettingsProcessor()), logger, workerDirectory);
                         var runtimeExecutable = new FileLocation(executableProcessor.GetEnvironmentExecutablePath("dotnet"));
                         frameworkServices.Logger.Info($"Running Canvas {_mode} {_version}");
                         logger.Info($"Command-line arguments: {string.Join(" ", _args)}");
@@ -83,91 +83,5 @@ namespace Canvas
         }
     }
 
-    public class NullExecutableProcessor : IExecutableProcessor
-    {
-        private ILogger _logger;
-        protected readonly List<IDirectoryLocation> _execDirs;
-
-        public NullExecutableProcessor(ILogger log, params IDirectoryLocation[] binaryDirs) 
-        {
-            _logger = log;
-            _execDirs = binaryDirs.ToList();
-        }
-
-        public string GetExecutableParameters(string executableFileNameWithoutExtension)
-        {
-            return null;
-        }
-
-        public string GetExecutable(string executableFileNameWithoutExtension, params string[] relativeBinDirectory)
-        {
-            return GetExecutableFileLocation(executableFileNameWithoutExtension, relativeBinDirectory).FullName;
-        }
-
-        public string GetEnvironmentExecutablePath(string executableFileNameWithoutExtension)
-        {
-            IFileLocation path;
-
-            // Now check the environment paths
-            var paths = Environment.GetEnvironmentVariable("PATH").Split(Path.PathSeparator).Select(dir => new DirectoryLocation(dir));
-            foreach (var candidatePath in paths)
-            {
-                var fullCandidatePath =
-                    GetPlatformDependentExecutablePath(candidatePath.GetFileLocation(executableFileNameWithoutExtension));
-
-                if (fullCandidatePath.Exists)
-                {
-                    return fullCandidatePath.FullName;
-                }
-            }
-            throw new Exception("Could not find executable '" + executableFileNameWithoutExtension + "' in environment path");
-        }
-
-        public bool TryGetCustomExecutableFileLocation(string executableFileNameWithoutExtension, out IFileLocation path)
-        {
-            path = null;
-            return false;
-        }
-        public bool TryGetCustomExecutablePath(string executableFileNameWithoutExtension, out string path)
-        {
-            path = null;
-            return false;
-        }
-
-
-        public IFileLocation GetExecutableFileLocation(string executableFileNameWithoutExtension, params string[] executableRelativePath)
-        {
-            IFileLocation path;
-
-            IDirectoryLocation SearchDir(IDirectoryLocation execDir) => executableRelativePath.Aggregate(execDir,
-                (dir, relativePath) => dir.GetDirectoryLocation(relativePath), result => result);
-
-            var execDirs = _execDirs.Select(SearchDir).ToList();
-            foreach (var execDir in execDirs)
-            {
-                path = GetPlatformDependentExecutablePath(
-                    execDir.GetFileLocation(executableFileNameWithoutExtension));
-                if (path.Exists) return path;
-            }
-            string searchedDirs = string.Join(", ", execDirs);
-            throw new Exception($"Could not find executable {executableFileNameWithoutExtension} in {searchedDirs}");
-        }
-
-        private IFileLocation GetPlatformDependentExecutablePath(IFileLocation executablePathWithoutExtension)
-        {
-            var path = executablePathWithoutExtension.AppendName(".exe");
-            if (path.Exists && !CrossPlatform.IsThisLinux())
-            {
-                return path;
-            }
-            path = executablePathWithoutExtension.AppendName(".dll");
-            if (path.Exists)
-            {
-                return path;
-            }
-            return executablePathWithoutExtension; // fallback
-        }
-
-    }
     
 }
