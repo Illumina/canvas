0529f68aa6081f6b833efca554d5a123d3be690b
Shuli Kang <skang@illumina.com>
2017-08-22 17:19:45 -0700

    comments removal

--- Src/Canvas/Canvas/CommandLineParsing/SingleSampleCommonOptionsParser.cs
+++ Src/Canvas/Canvas/CommandLineParsing/SingleSampleCommonOptionsParser.cs
@@ -7,12 +7,10 @@ namespace Canvas.CommandLineParsing
     {
         public const string SampleBAlleleVcfOptionName = "sample-b-allele-vcf";
         public const string PopulationBAlleleVcfOptionName = "population-b-allele-vcf";
-        //public const string PloidyBedOptionName = "ploidy-bed"; 
         public const string PloidyVcfOptionName = "ploidy-vcf";
         private static readonly FileOption SampleBAlleleSites = FileOption.Create("vcf containing SNV b-allele sites in the sample (only sites with PASS in the filter column will be used)", SampleBAlleleVcfOptionName);
         public static readonly FileOption PopulationBAlleleSites = FileOption.Create("vcf containing SNV b-allele sites in the population (only sites with PASS in the filter column will be used)", PopulationBAlleleVcfOptionName);
         private static readonly ExclusiveFileOption BAlleleSites = ExclusiveFileOption.CreateRequired(SampleBAlleleSites, PopulationBAlleleSites);
-        //private static readonly FileOption PloidyBed = FileOption.Create(".bed file containing regions of known ploidy in the sample. Copy number calls matching the known ploidy in these regions will be considered non-variant", PloidyBedOptionName);
         private static readonly FileOption PloidyVcf = FileOption.Create(".vcf file containing regions of known ploidy in the sample. Copy number calls matching the known ploidy in these regions will be considered non-variant", PloidyVcfOptionName);
         public static readonly StringOption SampleName = StringOption.CreateRequired("sample name", "n", "sample-name");
 
