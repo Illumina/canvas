56d58b2756e24b6bf31e89a824c0424be3316323
Eric Roller <eroller@illumina.com>
2017-10-03 15:14:13 -0700

    Testing new normalized coverage writer

--- Src/Canvas/CanvasTest/NormalizedCoverageWriterTests.cs
+++ Src/Canvas/CanvasTest/NormalizedCoverageWriterTests.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using CanvasCommon;
+using CanvasPedigreeCaller;
 using Illumina.Common;
-using Illumina.Common.FileSystem;
 using Isas.SequencingFiles;
 using Isas.SequencingFiles.Bed;
 using Xunit;
@@ -75,72 +73,6 @@ namespace CanvasTest
             }
 
             Assert.Equal("chr1\t0\t1\t1\nchr1\t1\t2\t1.5\n", coverageFile.ToString());
-        }
-    }
-
-    public class NormalizedCoverageWriter
-    {
-        public void Write(IEnumerable<CanvasSegment> segments, IFileLocation coverageFile)
-        {
-            var normalizationFactor = ComputeWeightedNormalizationFactor(segments);
-        }
-
-        /// <summary>
-        /// iterate through segments keeping a weighted sum of the normalization factor
-        /// </summary>
-        /// <param name="segments"></param>
-        /// <returns></returns>
-        private double ComputeWeightedNormalizationFactor(IEnumerable<CanvasSegment> segments)
-        {
-            var weightedSum = 0d;
-            foreach (var segment in segments)
-            {
-                var normalizationFactor = ComputeNormalizationFactor(segment);
-            }
-            return weightedSum;
-        }
-
-        private double ComputeNormalizationFactor(CanvasSegment segment)
-        {
-            throw new NotSupportedException();
-        }
-    }
-
-    public class BedGraphWriter
-    {
-        private readonly BgzipOrStreamWriter _writer;
-
-        public BedGraphWriter(BgzipOrStreamWriter writer)
-        {
-            _writer = writer;
-        }
-
-        public void WriteLine(BedGraphEntry entry)
-        {
-            var bedGraphLine = GetBedGraphLine(entry);
-            _writer.WriteLine(bedGraphLine);
-        }
-
-        private static string GetBedGraphLine(BedGraphEntry entry)
-        {
-            return $"{entry.Chromosome}\t{entry.Interval.Start}\t{entry.Interval.End}\t{entry.Value}";
-        }
-    }
-
-    public static class BedGraphWriterExtensions
-    {
-        public static void WriteLines(this BedGraphWriter writer, IEnumerable<BedGraphEntry> entries)
-        {
-            entries.ForEach(writer.WriteLine);
-        }
-
-        public static void WriteLines(this IFileLocation location, IEnumerable<BedGraphEntry> entries)
-        {
-            using (var streamWriter = new BgzipOrStreamWriter(location.FullName))
-            {
-                var writer = new BedGraphWriter(streamWriter);
-                writer.WriteLines(entries);
-            }
         }
     }
 }
