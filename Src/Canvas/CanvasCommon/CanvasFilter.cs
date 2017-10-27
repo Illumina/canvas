44d315db21198702156b9fbf6d53920c0fc47810
Shuli Kang <skang@illumina.com>
2017-10-27 14:35:24 -0700

    WIP

--- Src/Canvas/CanvasCommon/CanvasFilter.cs
+++ Src/Canvas/CanvasCommon/CanvasFilter.cs
@@ -7,54 +7,49 @@ namespace CanvasCommon
 {
     public class CanvasFilter
     {
-        public IReadOnlyList<string> FailedFilters { get; }
-
-        private CanvasFilter(IReadOnlyList<string> failedFilters)
-        {
-            FailedFilters = failedFilters;
-        }
-
-        public static CanvasFilter PassFilter = Create(Enumerable.Empty<string>());
-
-        public static CanvasFilter Create(IEnumerable<string> failedFilters)
-        {
-            var filters = new List<string>();
-            foreach (var filter in failedFilters)
-            {
-                if (filter == Pass)
-                    throw new IlluminaException($"{nameof(failedFilters)} cannot contain a filter named '{Pass}'");
-                if (filters.Contains(filter))
-                    throw new ArgumentException($"{nameof(failedFilters)} contains duplicate filter {filter}");
-                filters.Add(filter);
-            }
-            return new CanvasFilter(filters.ToReadOnlyList());
-        }
-
-        public string ToVcfString()
-        {
-            if (IsPass)
-                return Pass;
-            return string.Join(";", FailedFilters);
-        }
-
-        public bool IsPass => FailedFilters.Count == 0;
-
-        public static CanvasFilter GetSharedFilters(IReadOnlyList<CanvasFilter> sampleFilters)
-        {
-            var sharedFilters = sampleFilters.First().FailedFilters;
-            foreach (var sampleFilter in sampleFilters.Skip(1))
-            {
-                if (sharedFilters.Empty()) return PassFilter;
-                sharedFilters = sharedFilters.Intersect(sampleFilter.FailedFilters).ToList();
-            }
-            return Create(sharedFilters);
-        }
 
         public const int SegmantSizeCutoff = 10000;
         public const string Pass = "PASS";
         public static List<string> PassedFilter = new List<string> { Pass };
         public const string AllSampleFiltersFailed = "FailedFT";
         public const string SampleFilterFailed = "FT";
+        public IReadOnlyList<string> SetOfFailedFilters { get; }
+
+        private CanvasFilter(IReadOnlyList<string> setOfFailedFilters)
+        {
+            SetOfFailedFilters = setOfFailedFilters;
+        }
+
+        public static CanvasFilter PassFilter = Create(Enumerable.Empty<string>());
+
+        public static CanvasFilter Create(IEnumerable<string> setOfFailedFilters)
+        {
+            var allFailedFilters = new List<string>();
+            foreach (var failedFilters in setOfFailedFilters)
+            {
+                if (failedFilters == Pass)
+                    throw new IlluminaException($"{nameof(failedFilters)} cannot contain a filter named '{Pass}'");
+
+                allFailedFilters.Add(failedFilters);
+            }
+            return new CanvasFilter(allFailedFilters.ToReadOnlyList());
+        }
+
+        public string ToVcfString() => IsPass ? Pass : string.Join(";", SetOfFailedFilters);
+
+        public bool IsPass => SetOfFailedFilters.Count == 0;
+
+        public static CanvasFilter GetSharedFilters(IReadOnlyList<CanvasFilter> sampleFilters)
+        {
+            var sharedFilters = sampleFilters.First().SetOfFailedFilters;
+            foreach (var sampleFilter in sampleFilters.Skip(1))
+            {
+                if (sharedFilters.Empty()) return PassFilter;
+                sharedFilters = sharedFilters.Intersect(sampleFilter.SetOfFailedFilters).ToList();
+            }
+            return Create(sharedFilters);
+        }
+
         public static string FormatCnvSizeWithSuffix(int size)
         {
             if (size >= 1000000)
