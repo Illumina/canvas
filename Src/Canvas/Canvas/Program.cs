722f3122b3e678863b41a1727910eea685144c47
Eric Roller <eroller@illumina.com>
2018-01-22 10:18:12 -0800

    Catch unhandled exception

--- Src/Canvas/Canvas/Program.cs
+++ Src/Canvas/Canvas/Program.cs
@@ -8,14 +8,27 @@ namespace Canvas
     {
         public static int Main(string[] args)
         {
-            var mainParser = new MainParser(CanvasVersionInfo.VersionString, CanvasVersionInfo.CopyrightString,
-                new GermlineWgsModeParser("Germline-WGS", "CNV calling of a germline sample from whole genome sequencing data"),
-                new SomaticEnrichmentModeParser("Somatic-Enrichment", "CNV calling of a somatic sample from targeted sequencing data"),
-                new TumorNormalWgsModeParser("Somatic-WGS", "CNV calling of a somatic sample from whole genome sequencing data"),
-                new TumorNormalEnrichmentModeParser("Tumor-normal-enrichment", "CNV calling of a tumor/normal pair from targeted sequencing data"),
-                new SmallPedigreeModeParser("SmallPedigree-WGS", "CNV calling of a small pedigree from whole genome sequencing data"));
-            var result = mainParser.Run(args, Console.Out, Console.Error);
-            return result;
+            try
+            {
+                var mainParser = new MainParser(CanvasVersionInfo.VersionString, CanvasVersionInfo.CopyrightString,
+                    new GermlineWgsModeParser("Germline-WGS",
+                        "CNV calling of a germline sample from whole genome sequencing data"),
+                    new SomaticEnrichmentModeParser("Somatic-Enrichment",
+                        "CNV calling of a somatic sample from targeted sequencing data"),
+                    new TumorNormalWgsModeParser("Somatic-WGS",
+                        "CNV calling of a somatic sample from whole genome sequencing data"),
+                    new TumorNormalEnrichmentModeParser("Tumor-normal-enrichment",
+                        "CNV calling of a tumor/normal pair from targeted sequencing data"),
+                    new SmallPedigreeModeParser("SmallPedigree-WGS",
+                        "CNV calling of a small pedigree from whole genome sequencing data"));
+                var result = mainParser.Run(args, Console.Out, Console.Error);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return -1;
+            }
         }
     }
 }
