9b0ac48e847fdf6c210d071b14e30cfc1a17dca0
Eric Roller <eroller@illumina.com>
2018-02-08 12:11:59 -0800

    resharper fixes

--- Tools/EvaluateCNV/CNVChecker.cs
+++ Tools/EvaluateCNV/CNVChecker.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.IO;
 using CanvasCommon;
--- Tools/EvaluateCNV/Program.cs
+++ Tools/EvaluateCNV/Program.cs
@@ -6,7 +6,7 @@ using Illumina.Common.FileSystem;
 
 namespace EvaluateCNV
 {
-    class Program
+    internal static class Program
     {
         public static int Main(string[] args)
         {
