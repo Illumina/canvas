8dc32fc9654b2e111a05629f5b1ac1b5d8bb2ab7
Sergii Ivakhno <sivakhno@illumina.com>
2018-02-13 16:43:23 +0000

    more unit tests for GetGenotypeLogLikelihood

--- Src/Canvas/CanvasTest/CanvasPedigreeCaller/TestCopyNumberModels.cs
+++ Src/Canvas/CanvasTest/CanvasPedigreeCaller/TestCopyNumberModels.cs
@@ -52,5 +52,47 @@ namespace CanvasTest.CanvasPedigreeCaller
             Assert.True(diploidHet < lohB);
             Assert.True(diploidHet < lohA);
         }
+
+        [Fact]
+        public void haplotypeCopyNumberModelTester_Gain_CN4()
+        {
+            var copyNumberModelFactory = new HaplotypeCopyNumberModelFactory();
+            var copyNumberModel = copyNumberModelFactory.CreateModel(numCnStates: 5, maxCoverage: 300, meanCoverage: 100, diploidAlleleMeanCounts: 50.0);
+            var bAlleles = new Balleles(new List<Ballele>
+            {
+                new Ballele(1, 200, 1),
+                new Ballele(100, 202, 2),
+                new Ballele(200, 209, 3),
+                new Ballele(300, 1, 198),
+                new Ballele(400, 201, 2),
+                new Ballele(500, 199, 1)
+            });
+            double gt40 = copyNumberModel.GetGenotypeLogLikelihood(bAlleles, new PhasedGenotype(4, 0));
+            double gt31 = copyNumberModel.GetGenotypeLogLikelihood(bAlleles, new PhasedGenotype(3, 1));
+            double gt04 = copyNumberModel.GetGenotypeLogLikelihood(bAlleles, new PhasedGenotype(0, 4));
+            Assert.True(gt40 > gt31);
+            Assert.True(gt40 == gt04);
+        }
+
+        [Fact]
+        public void haplotypeCopyNumberModelTester_HetLoss()
+        {
+            var copyNumberModelFactory = new HaplotypeCopyNumberModelFactory();
+            var copyNumberModel = copyNumberModelFactory.CreateModel(numCnStates: 5, maxCoverage: 200, meanCoverage: 100, diploidAlleleMeanCounts: 50.0);
+            var bAlleles = new Balleles(new List<Ballele>
+            {
+                new Ballele(1, 31, 1),
+                new Ballele(100, 39, 2),
+                new Ballele(200, 33, 3),
+                new Ballele(300, 1, 33),
+                new Ballele(400, 36, 2),
+                new Ballele(500, 27, 1)
+            });
+            double gt10 = copyNumberModel.GetGenotypeLogLikelihood(bAlleles, new PhasedGenotype(1, 0));
+            double gt20 = copyNumberModel.GetGenotypeLogLikelihood(bAlleles, new PhasedGenotype(2, 0));
+            double gt01 = copyNumberModel.GetGenotypeLogLikelihood(bAlleles, new PhasedGenotype(0, 1));
+            Assert.True(gt10 > gt20);
+            Assert.True(gt10 == gt01);
+        }
     }
 }
